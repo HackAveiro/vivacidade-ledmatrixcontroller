2e189bf126b5e7336681eba3fcc72f6fafd627a1
rodrigograca31 <rodrigograca1993@hotmail.com>
2015-03-23 11:23:25 +0000

    Moving everything into files

---
+++ colorpickerraphael/js/script.js
@@ -0,0 +1,82 @@
+
+/**
+ * Function to map 0-360 to 0-255
+ * https://github.com/FastLED/FastLED/issues/147
+ */
+Number.prototype.map = function () {
+    return Math.round((this - 0) * (255 - 0) / (360 - 0) + 0);
+}
+
+Raphael(function () {
+    var reg = /^#(.)\1(.)\2(.)\3$/,
+            // this is where colorpicker created
+            cp = Raphael.colorpicker(0, 0, 200, "#eee", "picker");
+    clr = Raphael.color("#eee");
+
+    // assigning onchange event handler
+    cp.onchange = function (clr) {
+        var hsv_object = Colors.hex2hsv(clr);
+        console.log(hsv_object);
+
+        var send_this =
+                ("000" + hsv_object.H.map()).substr(-3, 3) +
+                ("000" + hsv_object.S.map()).substr(-3, 3) +
+                ("000" + hsv_object.V.map()).substr(-3, 3);
+
+        console.log(send_this);
+        publish(send_this, topic + 'bitmap', 2);
+    };
+});
+
+host = 'shineupon.me';
+port = 9001;
+topic = '/hackaveiro/';
+last_message_time = Date.now();
+
+//Using the HiveMQ public Broker, with a random client Id
+var client = new Messaging.Client(host, port, "myclientid_" + parseInt(Math.random() * 100, 10));
+
+//Gets  called if the websocket/mqtt connection gets disconnected for any reason
+client.onConnectionLost = function (responseObject) {
+    //Depending on your scenario you could implement a reconnect logic here
+    $("#messages").append("connection lost: " + responseObject.errorMessage);
+};
+
+//Gets called whenever you receive a message for your subscriptions
+client.onMessageArrived = function (message) {
+    //Do something with the push message you received
+    last_message_time = Date.now();
+    $('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span>');
+};
+
+//Connect Options
+var options = {
+    timeout: 3,
+    //Gets Called if the connection has sucessfully been established
+    onSuccess: function () {
+        $("#messages").append("Connected");
+        client.subscribe(topic + '#', {qos: 2});
+    },
+    //Gets Called if the connection could not be established
+    onFailure: function (message) {
+        $("#messages").append("Connection failed: " + message.errorMessage);
+    }
+};
+
+//Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
+var publish = function (payload, topic, qos) {
+    //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
+    var message = new Messaging.Message("" + payload);
+    message.destinationName = topic;
+    message.qos = qos;
+
+    if ((Date.now() - last_message_time) > 250) {
+        last_message_time = Date.now();
+        client.send(message);
+    }
+
+}
+
+$(document).ready(function () {
+    client.connect(options);
+});
