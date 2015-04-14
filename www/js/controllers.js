2910aaeef8ee682efa05ce91f946dcb92dd529c0
davidbeijinho <davidbeijinho@gmail.com>
2015-04-14 23:43:42 +0100

    add logic to ticatactoe, to change player color

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -60,7 +60,12 @@ angular.module('starter.controllers', [])
         }
     });
 }).controller('TicTacCtrl', function ($scope) {
-$scope.config={size:10};
+$scope.config={size:10,
+               nowPlayer:1,
+               color:['green','blue'],
+               turns:0
+
+                };
 
 
   $scope.board= new Array($scope.config.size-1);
@@ -74,14 +79,15 @@ for(var l=0;l<=$scope.config.size-1;l++){
   $scope.board[l][c]={color:'black'};
     }
 }
+$scope.paintBorder=function(idColor){
 
-/*
 for(var i=0;i<=$scope.config.size-1;i++){
-    $scope.board[0][i].color='red';//first line
-    $scope.board[i][0].color='red';//first collumn
-    $scope.board[i][$scope.config.size-1].color='red';//last collumn
-    $scope.board[$scope.config.size-1][i].color='red';//last line
-}*/
+    $scope.board[0][i].color=$scope.config.color[idColor];;//first line
+    $scope.board[i][0].color=$scope.config.color[idColor];;//first collumn
+    $scope.board[i][$scope.config.size-1].color=$scope.config.color[idColor];;//last collumn
+    $scope.board[$scope.config.size-1][i].color=$scope.config.color[idColor];;//last line
+}
+}
 
 for(var i=1;i<=$scope.config.size-2;i++){
      $scope.board[i][3].color='red';//fitst collumn
@@ -95,72 +101,88 @@ $scope.clickCell=function(id){
     console.log(id);
 
 if (id==11 || id==12 || id==21 || id==22){
-      $scope.board[1][1].color='blue';
-      $scope.board[1][2].color='blue';
-      $scope.board[2][1].color='blue';
-      $scope.board[2][2].color='blue';
+      $scope.board[1][1].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[1][2].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[2][1].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[2][2].color=$scope.config.color[$scope.config.nowPlayer];
+      
 }
 
 if (id==14 || id==15 || id==24 || id==25){
-      $scope.board[1][4].color='blue';
-      $scope.board[1][5].color='blue';
-      $scope.board[2][4].color='blue';
-      $scope.board[2][5].color='blue';
+      $scope.board[1][4].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[1][5].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[2][4].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[2][5].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
 if (id==17 || id==18 || id==27 || id==28){
-      $scope.board[1][7].color='blue';
-      $scope.board[1][8].color='blue';
-      $scope.board[2][7].color='blue';
-      $scope.board[2][8].color='blue';
+      $scope.board[1][7].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[1][8].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[2][7].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[2][8].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
 //----------------------------------------------------------------
 //----------------------------------------------------------------
 if (id==41 || id==42 || id==51 || id==52){
-      $scope.board[4][1].color='blue';
-      $scope.board[4][2].color='blue';
-      $scope.board[5][1].color='blue';
-      $scope.board[5][2].color='blue';
+      $scope.board[4][1].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[4][2].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[5][1].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[5][2].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
 if (id==44 || id==45 || id==54 || id==55){
-      $scope.board[4][4].color='blue';
-      $scope.board[4][5].color='blue';
-      $scope.board[5][4].color='blue';
-      $scope.board[5][5].color='blue';
+      $scope.board[4][4].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[4][5].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[5][4].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[5][5].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
 if (id==47 || id==48 || id==57 || id==58){
-      $scope.board[4][7].color='blue';
-      $scope.board[4][8].color='blue';
-      $scope.board[5][7].color='blue';
-      $scope.board[5][8].color='blue';
+      $scope.board[4][7].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[4][8].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[5][7].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[5][8].color=$scope.config.color[$scope.config.nowPlayer];
 }
 //----------------------------------------------------------------
 //----------------------------------------------------------------
 if (id==71 || id==72 || id==81 || id==82){
-      $scope.board[7][1].color='blue';
-      $scope.board[7][2].color='blue';
-      $scope.board[8][1].color='blue';
-      $scope.board[8][2].color='blue';
+      $scope.board[7][1].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[7][2].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[8][1].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[8][2].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
 if (id==74 || id==75 || id==84 || id==85){
-      $scope.board[7][4].color='blue';
-      $scope.board[7][5].color='blue';
-      $scope.board[8][4].color='blue';
-      $scope.board[8][5].color='blue';
+      $scope.board[7][4].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[7][5].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[8][4].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[8][5].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
 if (id==77 || id==78 || id==87 || id==88){
-      $scope.board[7][7].color='blue';
-      $scope.board[7][8].color='blue';
-      $scope.board[8][7].color='blue';
-      $scope.board[8][8].color='blue';
+      $scope.board[7][7].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[7][8].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[8][7].color=$scope.config.color[$scope.config.nowPlayer];
+      $scope.board[8][8].color=$scope.config.color[$scope.config.nowPlayer];
 }
 
+$scope.config.turns++;
+if ($scope.config.nowPlayer){
+  $scope.config.nowPlayer=0;
+}else{
+  $scope.config.nowPlayer=1;
 }
+
+
+if ($scope.config.turns==9){
+$scope.paintBorder( $scope.config.nowPlayer);
+}
+
+
+}
+
+
 
 console.log($scope.board);
 
